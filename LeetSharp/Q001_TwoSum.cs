de7541dd203c05c3aa6c690f44cab0976d126607
YiYang Li <yyli@ezesoft.com>
2015-02-05 13:50:18 -0600

    Q1, use either iteration O(n^2) or hash table O(n)

--- LeetSharp/Q001_TwoSum.cs
+++ LeetSharp/Q001_TwoSum.cs
@@ -21,7 +21,57 @@ namespace LeetSharp
     {
         public int[] TwoSum(int[] numbers, int target)
         {
+            var hashing = new HashSet<int>();
+            if (numbers.All(hashing.Add))
+                //If we could make sure that the elements are distinct in the IEnumerable<T>, we should leverage hash table 
+                return TwoSum2(numbers, target);
+            else
+            {
+                for (int i = 0; i < numbers.Count() - 1; i++)
+                    for (int j = i + 1; j < numbers.Count() - 1; j++)
+                        if (numbers[i] + numbers[j] == target)
+                            return new int[] { i + 1, j + 1 };
+            }
             return null;
+            
+        }
+        public int[] TwoSum2(int[] numbers, int target)
+        {
+            var hashing = numbers.Select((num, index) => new { Key = num, Value = index })
+                                .ToDictionary(a => a.Key, a => a.Value);
+            foreach (var number in numbers)
+            {
+                if (hashing.ContainsKey(target - numbers[hashing[number]]))
+                    return new int[] { hashing[number] + 1, hashing[target - numbers[hashing[number]]] + 1 };
+            }
+            return null;
+        }
+
+        public static void QSort(int[] numbers, int left, int right)
+        {
+            if (left >= right)
+                return;
+            Swap(ref numbers, left, left + (right - left) / 2);
+            int pivot = numbers[left];
+            int i = left + 1;
+            for (int j = i; j <= right; j++)
+            {
+                if (numbers[j] <= pivot)
+                {
+                    Swap(ref numbers, i, j);
+                    i++;
+                }
+            }
+            Swap(ref numbers, left, i - 1);
+            QSort(numbers, left, i - 1);
+            QSort(numbers, i, right);
+        }
+
+        public static void Swap(ref int[] numbers, int x, int y)
+        {
+            int temp = numbers[x];
+            numbers[x] = numbers[y];
+            numbers[y] = temp;
         }
 
         public string SolveQuestion(string input)
