9ecd92799bd12a1c12e09e1ab3bca846bcf5eb7c
YiYang Li <yyli@ezesoft.com>
2015-02-13 14:04:26 -0600

    complete Q4, which is same to the sample solution. Set left and right, and compare the middle (pivot) with the other array; if pivot is large, decrease it by moving to left, if pivot is small, increase it by moving to the right.

--- LeetSharp/Q004_MedianofTwoSortedArrays.cs
+++ LeetSharp/Q004_MedianofTwoSortedArrays.cs
@@ -15,7 +15,64 @@ namespace LeetSharp
     {
         public double FindMedianSortedArrays(int[] a, int[] b)
         {
-            return -1f;
+            int totalLen = a.Length + b.Length;
+            int midIndex1 = (totalLen - 1) / 2;
+            int midIndex2 = totalLen / 2;
+            if (a.Length == 0)
+                return (double)(b[midIndex1] + b[midIndex2]) / 2.0;
+            if (b.Length == 0)
+                return (double)(a[midIndex1] + a[midIndex2]) / 2.0;
+            if (midIndex1 == midIndex2)
+                return FindKthSmallest(a, b, midIndex1);
+            else
+                return (double)(FindKthSmallest(a, b, midIndex1) + FindKthSmallest(a, b, midIndex2)) / 2.0;
+        }
+
+        private T FindKthSmallest<T>(T[] a, T[] b, int k)
+            where T : IComparable, IComparable<int>
+        {
+            int left = 0;
+            int right = a.Length - 1;
+            while (left <= right)
+            {
+                int indexA = (left + right) / 2;
+                int indexB = k - indexA - 1;
+                if (indexB >= b.Length)
+                {
+                    //increase indexA (move to right part) to get some B
+                    left = indexA + 1;
+                    continue;
+                }
+                if (indexB < -1)
+                {
+                    //decrease indexA (move to left part) to get some B
+                    right = indexA - 1;
+                    continue;
+                }
+                if (indexB == -1)
+                {
+                    //indexA is kth between left and right, but we need to make sure, pivot is less than B[0], unless we need to decrease the pivot. 
+                    if (a[indexA].CompareTo(b[0]) <= 0)
+                        return a[indexA];
+                    else
+                        right = indexA - 1;
+                    continue;
+                }
+                if (a[indexA].CompareTo(b[indexB]) >=0 )
+                {
+                    //if the pivot of A is larger than the left part of B, we decrease the pivot and include more B next time. 
+                    if (indexB == b.Length - 1 || a[indexA].CompareTo(b[indexB + 1]) <= 0)
+                        return a[indexA];
+                    else
+                        right = indexA - 1;
+                }
+                else
+                {
+                    //if the pivot of A is less than B, we increase pivot. 
+                    left = indexA + 1;
+                }
+            }
+            return FindKthSmallest(b, a, k);
         }
 
         public string SolveQuestion(string input)
