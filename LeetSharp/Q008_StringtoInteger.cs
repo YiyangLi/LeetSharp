b1d3266d4147d531c3a3a20053782d8f7ea0e8a8
YiYang Li <yyli@ezesoft.com>
2015-02-17 14:04:13 -0600

    Complete Q8, take care of the invalid input, boundary problem, use long to store the result then convert to the final result.

--- LeetSharp/Q008_StringtoInteger.cs
+++ LeetSharp/Q008_StringtoInteger.cs
@@ -14,7 +14,30 @@ namespace LeetSharp
     {
         public int Atoi(string str)
         {
-            return -1;
+            if (string.IsNullOrWhiteSpace(str))
+                return 0;
+            str = str.Trim();
+            long result = 0;
+            bool isNegative = false;
+            if (str[0] == '-' || str[0] == '+')
+            {
+                isNegative = str[0] == '-';
+                str = str.Substring(1);
+            }
+            foreach (var c in str)
+            {
+                if (c < '0' || c > '9')
+                {
+                    break;
+                }
+                result = result * 10;
+                result = result + c - '0';
+                if (result > int.MaxValue)
+                {
+                    return isNegative ? int.MinValue : int.MaxValue;
+                }
+            }
+            return (int)(isNegative ? (-1) * result : result);
         }
 
         public string SolveQuestion(string input)
