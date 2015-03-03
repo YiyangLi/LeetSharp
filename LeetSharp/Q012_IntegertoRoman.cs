35f2a1d0947ccfa03ad52e5e669db56be82ce2d4
YiYang Li <yyli@ezesoft.com>
2015-03-03 13:18:04 -0600

    complete Q12

--- LeetSharp/Q012_IntegertoRoman.cs
+++ LeetSharp/Q012_IntegertoRoman.cs
@@ -16,7 +16,57 @@ namespace LeetSharp
     {
         public string IntToRoman(int num)
         {
-            return null;
+            List<string> result = new List<string>();
+            int thousands = num / 1000;
+            int hundreds = num % 1000 / 100;
+            int tens = num % 100 / 10;
+            int digits = num % 10;
+            if (thousands > 0)
+            { //either 1, 2, or 3, since the max is 3999. 
+                result.Add(new string('M', thousands));
+            }
+            if (hundreds > 0)
+            { //0 - 9
+                result.Add(Roman(hundreds, 'C', 'D', 'M'));
+            }
+            if (tens > 0)
+            { //0 - 9
+                result.Add(Roman(tens, 'X', 'L', 'C'));
+            }
+            if (digits > 0)
+            { //0 - 9
+                result.Add(Roman(digits, 'I', 'V', 'X'));
+            }
+            return string.Concat(result);
+        }
+
+        public string Roman(int num, char pre, char mid, char next)
+        {
+            string result = string.Empty;
+            switch (num)
+            { 
+                case 1:
+                case 2:
+                case 3:
+                    result = new string(pre, num);
+                    break;
+                case 4:
+                    result = new string(new[] { pre, mid });
+                    break;
+                case 5:
+                case 6:
+                case 7:
+                case 8:
+                    result = string.Concat(mid, new string(pre, num - 5));
+                    break;
+                case 9:
+                    result = new string(new[] { pre, next });
+                    break;
+                default:
+                    //error
+                    break;
+            }
+            return result;
         }
 
         public string SolveQuestion(string input)
